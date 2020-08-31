cbce0f762fd046807c3aa278ff88a45379b79af9
Zubair Beg <ziibii88@gmail.com>
2020-08-31 16:30:38 +0800

    added react-router-dom package

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
+import {Route, Switch} from "react-router-dom";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
 
 class App extends React.Component {
   render() {
     return (
         <Layout>
-            <BurgerBuilder />
+            <Switch>
+                <Route path="/checkout" component={Checkout} />
+                <Route path="/" component={BurgerBuilder} />
+            </Switch>
         </Layout>
     );
   }
